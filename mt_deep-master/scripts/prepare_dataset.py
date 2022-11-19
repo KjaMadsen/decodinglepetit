c6d7ebf13195da9814bc36a87efd1f377702cc7f
KjaMadsen <96437947+KjaMadsen@users.noreply.github.com>
2022-11-19 15:13:27 +0100

    prepare dataset ready

--- mt_deep-master/scripts/prepare_dataset.py
+++ mt_deep-master/scripts/prepare_dataset.py
@@ -1,22 +1,28 @@
-# %%
 #Train, val, Test splitter
 import os
-from sklearn.model_selection import train_test_split
 import shutil
 import random
 
-# %%
+
 #Structure
 #
-#data - raw_data 
+# data - raw_data/derivatives
+# |
+# Train - Val - Test
 # |
 # run (0,1,2,3,...)
 # |
-# CN - EN - FR + labels.json
+# CN - EN - FR
 # |
-# MRI-files 
+# MRI-files and labels.txt
 
-unsorted_data_dir = ""
+def shuffle_dict(dictionary):
+    keys = list(dictionary.keys())
+    random.shuffle(keys)
+    shuffled_dict = {}
+    for key in keys:
+        shuffled_dict.update({key: dictionary[key]})
+    return shuffled_dict
 
 def get_all_data(unsorted_data_dir):
     data = []
@@ -26,7 +32,7 @@ def get_all_data(unsorted_data_dir):
                         data.append((run, os.path.join(dirpath, scan)))
     return data
 
-def sort_by_subject(data, add_run=False):
+def sort_by_subject(data, unsorted_data_dir, add_run=False):
     output = {sub: [] for sub in os.listdir(unsorted_data_dir)}
     for run,file in data:
         if add_run:
@@ -35,7 +41,7 @@ def sort_by_subject(data, add_run=False):
             output[file[len(unsorted_data_dir):len(unsorted_data_dir)+9]].append(file)
     return {k:v for k,v in output.items() if len(v)>0}
 
-def sort_by_language(data):
+def sort_by_language(data, unsorted_data_dir):
     output = {"EN":[], "CN":[], "FR":[]}
     for run,file in data:
         output[file[len(unsorted_data_dir)+4:len(unsorted_data_dir)+6]].append((run,file))
@@ -44,43 +50,45 @@ def sort_by_language(data):
 
 #config 1: same language, different subjects
 #obs: need to make sure that the same subject is not in train/val/test
-def config1(data, language = "CN", split=(0.8,0.1,0.1)):
-    relevant_files = sort_by_subject(sort_by_language(data)[language], add_run=True)
-    random.shuffle(relevant_files)
+def config1(data, unsorted_data_dir, language = "CN", split=(0.8,0.1,0.1)):
+    relevant_files = sort_by_subject(sort_by_language(data, unsorted_data_dir)[language], unsorted_data_dir, add_run=True)
+    relevant_files = shuffle_dict(relevant_files)
     split_ = (int(len(relevant_files)*split[0]), int(len(relevant_files)*split[0])+int(len(relevant_files)*split[1])) 
     train = [f for f in list(relevant_files.keys())[:split_[0]]]
     val = [f for f in list(relevant_files.keys())[split_[0]:split_[1]]]
     test = [f for f in list(relevant_files.keys())[split_[1]:]]
     folders = {"Train":train, "Val":val, "Test":test}
-    for k, subs in folders.items():
+    for partition, subs in folders.items():
         for sub in subs:
             for run, file in relevant_files[sub]:
-                if not os.path.exists(f"data/{k}/{run}/{language}/"):
-                    os.makedirs(f"data/{k}/{run}/{language}/")
-                shutil.copy(file, f"data/{k}/{run}/{language}/")
+                if not os.path.exists(f"data/{partition}/{run}/{language}/"):
+                    os.makedirs(f"data/{partition}/{run}/{language}/")
+                shutil.copy(file, f"data/{partition}/{run}/{language}/")
+                print(file)
     
 #config 2: same language, same subject
 #use only one subject to train/val/test
 #obs: need to make sure the labels exists in train/val/test
-
-def config2(data, language = "CN"):
-    relevant_files = sort_by_subject(sort_by_language(data)[language])
-    subject = random.choice(list(relevant_files.values())) #choose a random subject for training
+def config2(data, unsorted_data_dir, language = "CN"):
+    relevant_files = sort_by_subject(sort_by_language(data, unsorted_data_dir)[language], unsorted_data_dir)
+    subject_name = random.choice(list(relevant_files.keys())) #choose a random subject for training
+    subject = relevant_files[subject_name]
     random.shuffle(subject) 
     train = subject[:len(subject)-1]
-    val = subject[-2]
-    test = subject[-1]
+    val = [subject[-2]]
+    test = [subject[-1]]
     folders = {"Train":train, "Val":val, "Test":test}
     for k, v in folders.items():
         for file in v:
-            if not os.path.exists(f"data/{k}/{subject}"):
-                os.makedirs(f"data/{k}/{subject}")
-            shutil.copy(file, f"data/{k}/{subject}/")
+            if not os.path.exists(f"data/{k}/{subject_name}"):
+                os.makedirs(f"data/{k}/{subject_name}")
+            shutil.copy(file, f"data/{k}/{subject_name}/")
+            print(file)
     
 #config 3: different language
-def config3(data, train_language = "CN", test_language = "EN"):
-    train_files = sort_by_language(data)[train_language]
-    test_files = sort_by_language(data)[test_language]
+def config3(data, unsorted_data_dir, train_language = "CN", test_language = "EN"):
+    train_files = sort_by_language(data, unsorted_data_dir)[train_language]
+    test_files = sort_by_language(data, unsorted_data_dir)[test_language]
 
     for run, file in train_files:
         if not os.path.exists(f"data/Train/{run}/{train_language}/"):
@@ -103,16 +111,25 @@ def main():
     #split = (0.8,0.1,0.1)
     random.seed(1234)
     #__init__
-    for dirpath,s,files in os.walk("data/"):
-        for run, scan in enumerate(sorted(files)):
-            if scan.endswith(".nii.gz"): #maybe include labels?
-                os.remove(os.path.join(dirpath, scan))
+    print("\nRemoving folders...")
+    for i in os.listdir("data/"):
+        shutil.rmtree("data/"+i)
     
+    #for dirpath,s,files in os.walk("data/"):
+    #    for run, scan in enumerate(sorted(files)):
+    #        if scan.endswith(".nii.gz"): #maybe include labels?
+    #            os.remove(os.path.join(dirpath, scan)) #clear train/val/test folders
+    
+    print("Done removing folders")
+    print("\nRetriving data...")
     data = get_all_data(unsorted_data_dir)
-    # config1(data, language="CN")
-    # config2(data, language="CN")
+    print("Done retriving data")
+    print("Moving data into folders....")
+    # config1(data, unsorted_data_dir, language="CN")
+    # config2(data, unsorted_data_dir, language="CN")
     # config3(data, train_language="CN", test_language="EN")
-    return
+    print("Done!")
+    return 0
 
 if __name__=="__main__":
     main()
